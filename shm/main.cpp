eb97c1c8d5a5a06d39789f1efb9f8658de710638
kbdyj <kbdyj1@gmail.com>
2026-05-26 21:44:39 +0900

    [os] test pthread

--- shm/main.cpp
+++ shm/main.cpp
@@ -2,13 +2,19 @@ void test_consumer_producer();
 void test_shm();
 int test_pipe();
 int test_fork();
+void test_pthread(int argc, char* argv[]);
 
-int main()
+int main(int argc, char* argv[])
 {
+    (void)argc;
+    (void)argv;
+
     //test_consumer_producer();
     //test_shm();
     //test_pipe();
-    test_fork();
+    //test_fork();
+
+    test_pthread(argc, argv);
 
     return 0;
 }
